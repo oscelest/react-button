8bb5b656713acf5212e0caad83b7c702b7b6ef33
oscelest <oscelest@gmail.com>
2022-11-16 19:44:09 +0100

    Update to add disabled button possibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import {Button} from "../src";
 function IndexPage() {
   return (
     <div style={{display: "flex", gridRowGap: "10px", flexFlow: "column", width: "100%", height: "500px"}}>
+      <Button icon={"\\1F5F8"} disabled={true}>Hello World</Button>
       <Button icon={"\\1F5F8"}>Hello World</Button>
     </div>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, {ReactNode, CSSProperties, useState} from "react";
+import React, {CSSProperties, ReactNode, useState} from "react";
 import Style from "./Button.module.css";
 
 function Button<T>(props: ButtonProps<T>) {
@@ -6,16 +6,17 @@ function Button<T>(props: ButtonProps<T>) {
   const [mouse_down, setMouseDown] = useState<boolean>(false);
   const [hover, setHover] = useState<boolean>(false);
   const [focus, setFocus] = useState<boolean>(false);
-  const {value, children, icon, onClick, ...component_props} = props;
+  const {className, value, disabled, tabIndex, children, icon, onClick, ...component_props} = props;
 
   const classes = [Style.Component, "button"];
-  if (props.className) classes.push(props.className);
-
-  const icon_style = props.icon ? {"--icon": `"${icon}"`} as CSSProperties : undefined;
+  if (className) classes.push(className);
+  
+  const icon_style = icon ? {"--icon": `"${icon}"`} as CSSProperties : undefined;
   const active = key_down || mouse_down;
+  const tab_index = !disabled ? tabIndex : undefined;
 
   return (
-    <div {...component_props} className={classes.join(" ")} tabIndex={0} data-active={active} data-hover={hover} data-focus={focus}
+    <div {...component_props} className={classes.join(" ")} tabIndex={tab_index} data-active={active} data-hover={hover} data-focus={focus} data-disabled={disabled}
          onClick={onButtonClick} onMouseEnter={onButtonMouseEnter} onMouseLeave={onButtonMouseLeave} onMouseDown={onButtonMouseDown} onMouseUp={onButtonMouseUp}
          onFocus={onButtonFocus} onBlur={onButtonBlur} onKeyDown={onButtonKeyDown} onKeyUp={onButtonKeyUp}>
       {!!icon_style && <div className={"button-icon"} style={icon_style}></div>}
@@ -24,50 +25,50 @@ function Button<T>(props: ButtonProps<T>) {
   );
 
   function onButtonClick(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     onClick?.(event, value);
   }
 
   function onButtonFocus(event: React.FocusEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setFocus(true);
   }
 
   function onButtonBlur(event: React.FocusEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setFocus(false);
   }
 
   function onButtonMouseEnter(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setHover(true);
   }
 
   function onButtonMouseLeave(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setHover(false);
   }
 
   function onButtonMouseDown(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setMouseDown(true);
     props.onMouseDown?.(event);
   }
 
   function onButtonMouseUp(event: React.MouseEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     setMouseDown(false);
     props.onMouseUp?.(event);
   }
 
   function onButtonKeyDown(event: React.KeyboardEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     if (event.code === "Enter" || event.code === "NumpadEnter" || event.code === "Space") setKeyDown(true);
     props.onKeyDown?.(event);
   }
 
   function onButtonKeyUp(event: React.KeyboardEvent<HTMLDivElement>) {
-    if (event.defaultPrevented) return;
+    if (event.defaultPrevented || disabled) return;
     if (event.code === "Enter" || event.code === "NumpadEnter" || event.code === "Space") setKeyDown(false);
     props.onKeyUp?.(event);
   }
